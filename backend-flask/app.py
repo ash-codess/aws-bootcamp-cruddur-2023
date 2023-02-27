fcdbc75bbca5e35175752f0aab00c2a20372d695
ash-codess <asthaghosh.it@gmail.com>
2023-02-27 06:18:05 +0000

    added segments-wip

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -34,7 +34,7 @@ provider.add_span_processor(processor)
 
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+
 
 # Show this in the logs within the backend-flask app (STDOUT)
 simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
@@ -45,6 +45,8 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+XRayMiddleware(app, xray_recorder)
 
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,10 +1,13 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
+
 class UserActivities:
   def run(user_handle):
+    segment = xray_recorder.begin_segment('user_activities')
     model = {
       'errors': None,
       'data': None
-    }
+      }
 
     now = datetime.now(timezone.utc).astimezone()
 
@@ -20,4 +23,13 @@ class UserActivities:
         'expires_at': (now + timedelta(days=31)).isoformat()
       }]
       model['data'] = results
+
+    subsegment = xray_recorder.begin_subsegment('mock-data')
+    # xray ---
+    dict = {
+      "now": now.isoformat(),
+      "results-size": len(model['data'])
+    }
+    subsegment.put_metadata('key', dict, 'namespace')
+
     return model
