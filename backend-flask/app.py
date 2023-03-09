ae25985f50ef7a11d848b0cecbc0bb175d8a6836
ash-codess <asthaghosh.it@gmail.com>
2023-03-09 15:11:53 +0000

    cors-error

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -88,9 +88,10 @@ RequestsInstrumentor().instrument()
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
+
 cors = CORS(
   app, 
-  resources={r"/api/*": {"origins": origins}},
+  resources={r"/api/*": {"origins": origins}}, 
   headers=['Content-Type', 'Authorization'], 
   expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
