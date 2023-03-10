2f56523c68d9fc81a1b1d0fd7f5fd655e41ac7c6
ash-codess <asthaghosh.it@gmail.com>
2023-03-10 05:31:04 +0000

    issue#2

--- backend-flask/lib/cognito_jwt_token.py
+++ backend-flask/lib/cognito_jwt_token.py
@@ -33,7 +33,7 @@ class CognitoJwtToken:
 
 
     def _load_jwk_keys(self):
-        keys_url = f"https://cognito-idp.{self.region}.amazonaws.com/{self.user_pool_id}/.well-known/jwks.json"
+        keys_url = f"https://cognito-idp.us-east-1.amazonaws.com/us-east-1_FRUwQ9XYn/.well-known/jwks.json"
         try:
             response = self.request_client(keys_url)
             self.jwk_keys = response.json()["keys"]
